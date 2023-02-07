87f91daada6751b911975f770aee4e0170b11714
Danya <aparshin@2002gmail.com>
2023-02-08 01:20:48 +0300

    задание 29

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.\
+int[] array = new int [8];
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"введите элемент массива {i}:\t");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.WriteLine(array[i]);
+}
